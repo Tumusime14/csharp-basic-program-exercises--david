876ba0e932968d76862f3d7c3b2c1a46acc5d88b
tumusime_david <tumusimedavid14@gmail.com>
2024-08-09 17:20:55 +0200

    Fourth task, Swapping numbers

---
+++ SwapValues/ValuesSwap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwapValues
+{
+    class ValuesSwap
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Console.WriteLine($"After swapping: Number 1 = {number1}, Number 2 = {number2}");
+        }
+    }
+}
