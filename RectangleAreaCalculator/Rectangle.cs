0b780286d7c16c57f46e3a115e7e31e9c0435634
tumusime_david <tumusimedavid14@gmail.com>
2024-08-09 17:18:09 +0200

    Third task, Rectangle area calculated

---
+++ RectangleAreaCalculator/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectangleAreaCalculator
+{
+    class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width of the rectangle: ");
+            string widthInput = Console.ReadLine();
+
+            Console.Write("Enter the length of the rectangle: ");
+            string lengthInput = Console.ReadLine();
+
+            double width = Convert.ToDouble(widthInput);
+            double length = Convert.ToDouble(lengthInput);
+
+            double area = width * length;
+
+            Console.WriteLine($"The area of the rectangle is {area} square units.");
+        }
+    }
+}
