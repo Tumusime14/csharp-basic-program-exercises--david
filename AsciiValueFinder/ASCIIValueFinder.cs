e876972a8cffe52c3ff5a4a6a756f432787b9131
tumusime_david <tumusimedavid14@gmail.com>
2024-08-09 17:05:45 +0200

    Second task ASCII Finder for user input Char

---
+++ AsciiValueFinder/ASCIIValueFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AsciiValueFinder
+{
+    class ASCIIValueFinder
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character: ");
+            char userChar = Console.ReadKey().KeyChar;
+            int asciiValue = (int)userChar;
+            Console.WriteLine($"\nThe ASCII value of '{userChar}' is {asciiValue}.");
+        }
+    }
+}
