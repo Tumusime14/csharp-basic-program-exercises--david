9db34bd2d7602fa5bcb54b10a61cbfdde53aecb1
tumusime_david <tumusimedavid14@gmail.com>
2024-08-09 17:02:22 +0200

    First task converting celsius into fahrenheit

---
+++ TemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class TemperatureConverter
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature in Celsius: ");
+
+            string celsuis = Console.ReadLine();
+
+            double celsius = Convert.ToDouble(celsuis);
+            double fahrenheit = (celsius * 1.8) + 32;
+            Console.WriteLine($"{celsius}°C is equal to {fahrenheit}°F");
+
+        }
+    }
+}
